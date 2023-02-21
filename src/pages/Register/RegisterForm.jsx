a26087195d9940fe333c99e95248cb890df1aa18
Ranayna <ranayna-oliveira@outlook.com>
2023-02-21 17:56:21 -0300

    feat: campos de senha na mesma linha

--- src/pages/Register/RegisterForm.jsx
+++ src/pages/Register/RegisterForm.jsx
@@ -2,43 +2,66 @@ import React from 'react';
 import { useForm} from 'react-hook-form';
 
 import {
+  Box,
+  Checkbox,
+  Flex,
   FormControl,
   FormLabel,
   FormErrorMessage,
   FormHelperText,
-  Input
+  Input,
+  Spacer,
 } from '@chakra-ui/react'
 
 function RegisterForm() {
-  const {register, handleSubmit, control} = useForm({
-    defaultValues:{
-      firstName:'',
-      lastName:'',
-      email:'',
-      password:'',
-      confirmedPassword:'',
-    }
-  });
+  const {register, handleSubmit} = useForm();
   
   const onSubmit = (data)=>{
     console.log(data);
   }
+
+  const errorValidation = {
+    names: {
+      minLength: {
+        value: 2,
+        message: 'O campo possui menos de 2 caracteres.'
+      },
+      maxLength: {
+        value: 60,
+        message: 'O campo possui mais de 60 caracteres.'
+      }
+    },
+    email:{     
+    } 
+  }
+
+  
   
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <FormControl >
+
         <FormLabel>Nome</FormLabel>
         <Input  
-          {...register('firstName')}
           id='firstName'
           placeholder='Digite o seu nome'
+          {...register('firstName',
+          {
+            required: true,
+            minLength: {
+              value: 2
+            },
+            maxLength: 60}
+            )}
         />
+
         <FormLabel>Sobrenome</FormLabel>
         <Input  
           {...register('lastName')}
           id='lastName'
           placeholder='Digite o seu sobrenome'
         />
+
         <FormLabel>E-mail</FormLabel>
         <Input  
           {...register('email')}
@@ -46,24 +69,49 @@ function RegisterForm() {
           type='email'
           placeholder='Digite o seu e-mail'
         />
-        <FormLabel>Senha</FormLabel>
+        <Flex gap='2'>
+          <Box flex='1'>
+            <FormLabel>Senha</FormLabel>
+              <Input  
+                id='password'
+                {...register('password')}
+                type='password'
+              />
+          </Box>
+          <Box flex='1'>
+            <FormLabel>Confirme a senha</FormLabel>
+            <Input  
+              {...register('confirmedPass')}
+              id='confirmedPassword'
+              type='password'
+            />
+          </Box>
+        </Flex>
+
+        <FormLabel>Possui deficiência? Se sim, qual?</FormLabel>
         <Input  
-          id='password'
-          {...register('password')}
-          type='password'
-        />
-        <FormLabel>Confirme a senha</FormLabel>
-        <Input  
-          {...register('confirmedPass')}
-          id='confirmedPassword'
-          type='confirmedPassword'
+          {...register('haveDisability')}
+          id='haveDisability'
+          type='text'
         />
 
-        <Input type='submit' value='Enviar'/>
-    </FormControl>
+        <FormLabel>É especialista em alguma deficiência? Se sim, qual?</FormLabel>
+        <Input  
+          {...register('isSpecialist')}
+          id='isSpecialist'
+          type='text'
+        />
+        
+        <Checkbox id='checkbox'>
+          Li e aceito os termos e políticas do Portal Acessibilidade Dev
+        </Checkbox>
+        
+        <Input type='submit' value='Enviar' color='blue' />
+     
+      </FormControl>
     </form>
     
   )
 }
 
-export default RegisterForm
+export default RegisterForm;
