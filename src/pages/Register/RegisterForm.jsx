03a23ccd70ec277dde688c5528b79e1bf9f68d19
Luan Teixeira <luanlts@outlook.com>
2023-02-21 21:46:01 -0300

    feat: adiciona regras de validacao form cadastro

--- src/pages/Register/RegisterForm.jsx
+++ src/pages/Register/RegisterForm.jsx
@@ -1,117 +1,222 @@
-import React from 'react';
-import { useForm} from 'react-hook-form';
+import { useForm } from "react-hook-form";
+
+import styles from "./styles.module.css";
 
 import {
+  Alert,
+  AlertIcon,
+  AlertDescription,
   Box,
   Checkbox,
   Flex,
   FormControl,
-  FormLabel,
   FormErrorMessage,
   FormHelperText,
+  FormLabel,
   Input,
-  Spacer,
-} from '@chakra-ui/react'
+} from "@chakra-ui/react";
 
 function RegisterForm() {
-  const {register, handleSubmit} = useForm();
-  
-  const onSubmit = (data)=>{
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm();
+
+  const onSubmit = (data) => {
     console.log(data);
-  }
+  };
 
   const errorValidation = {
     names: {
       minLength: {
         value: 2,
-        message: 'O campo possui menos de 2 caracteres.'
+        message: "O campo não possui o mínimo de caracteres.",
       },
       maxLength: {
         value: 60,
-        message: 'O campo possui mais de 60 caracteres.'
-      }
+        message: "O campo ultrapassa o limite de caracteres.",
+      },
     },
-    email:{     
-    } 
-  }
+    email: {
+      pattern: {
+        value: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/,
+        message: "O e-mail está fora do padrão. Exemplo: exemplo@email.com.br",
+      },
+      minLength: {
+        value: 4,
+        message: "O campo não possui o mínimo de caracteres.",
+      },
+      maxLength: {
+        value: 40,
+        message: "O campo ultrapassa o limite de caracteres.",
+      },
+    },
+    password: {
+      pattern: {
+        value:
+          /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,30}$/,
+        message: "A senha está fora do padrão sugerido.",
+      },
+    },
+    filled: {
+      required: "Este campo é obrigatório!",
+    },
+  };
 
-  
-  
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <FormControl >
-
-        <FormLabel>Nome</FormLabel>
-        <Input  
-          id='firstName'
-          placeholder='Digite o seu nome'
-          {...register('firstName',
-          {
-            required: true,
-            minLength: {
-              value: 2
-            },
-            maxLength: 60}
-            )}
+      <FormControl isRequired isInvalid={errors.firstName}>
+        <FormLabel htmlFor="firstName" className={styles.registerLabels}>
+          Nome
+        </FormLabel>
+        <Input
+          id="firstName"
+          placeholder="Digite o seu nome"
+          isInvalid={errors.firstName ? true : false}
+          {...register("firstName", {
+            ...errorValidation.names,
+            ...errorValidation.filled,
+          })}
         />
+        {!errors.firstName ? (
+          <FormHelperText>asdadad</FormHelperText>
+        ) : (
+          <FormErrorMessage>{errors.firstName.message}</FormErrorMessage>
+        )}
+      </FormControl>
 
-        <FormLabel>Sobrenome</FormLabel>
-        <Input  
-          {...register('lastName')}
-          id='lastName'
-          placeholder='Digite o seu sobrenome'
+      <FormControl isRequired isInvalid={errors.lastName}>
+        <FormLabel htmlFor="lastName" className={styles.registerLabels}>
+          Sobrenome
+        </FormLabel>
+        <Input
+          id="lastName"
+          placeholder="Digite o seu sobrenome"
+          isInvalid={errors.lastName ? true : false}
+          {...register("lastName", {
+            ...errorValidation.names,
+            ...errorValidation.filled,
+          })}
         />
+        {!errors.lastName ? (
+          <FormHelperText>
+            O campo deve possui no mínimo 2 de caracteres.
+          </FormHelperText>
+        ) : (
+          <FormErrorMessage>{errors.lastName.message}</FormErrorMessage>
+        )}
+      </FormControl>
 
-        <FormLabel>E-mail</FormLabel>
-        <Input  
-          {...register('email')}
-          id='email'
-          type='email'
-          placeholder='Digite o seu e-mail'
+      <FormControl isRequired isInvalid={errors.email}>
+        <FormLabel htmlFor="email" className={styles.registerLabels}>
+          E-mail
+        </FormLabel>
+        <Input
+          id="email"
+          type="email"
+          placeholder="Digite o seu e-mail"
+          isInvalid={errors.email ? true : false}
+          {...register("email", {
+            ...errorValidation.email,
+            ...errorValidation.filled,
+          })}
         />
-        <Flex gap='2'>
-          <Box flex='1'>
-            <FormLabel>Senha</FormLabel>
-              <Input  
-                id='password'
-                {...register('password')}
-                type='password'
-              />
-          </Box>
-          <Box flex='1'>
-            <FormLabel>Confirme a senha</FormLabel>
-            <Input  
-              {...register('confirmedPass')}
-              id='confirmedPassword'
-              type='password'
+        {!errors.email ? (
+          <FormHelperText>Exemplo: exemplo@email.com.br</FormHelperText>
+        ) : (
+          <FormErrorMessage>{errors.email.message}</FormErrorMessage>
+        )}
+      </FormControl>
+
+      <Flex gap="2">
+        <Box flex="1">
+          <FormControl isRequired isInvalid={errors.password}>
+            <FormLabel htmlFor="password" className={styles.registerLabels}>
+              Senha
+            </FormLabel>
+            <Input
+              isInvalid={errors.password ? true : false}
+              id="password"
+              type="password"
+              {...register("password", {
+                ...errorValidation.password,
+                ...errorValidation.filled,
+              })}
             />
-          </Box>
-        </Flex>
+            {!errors.password ? (
+              <FormHelperText>
+                A senha deve conter no mínimo 8 caracteres.
+              </FormHelperText>
+            ) : (
+              <FormErrorMessage>{errors.password.message}</FormErrorMessage>
+            )}
+          </FormControl>
+        </Box>
+        <Box flex="1">
+          <FormControl isRequired isInvalid={errors.password}>
+            <FormLabel
+              htmlFor="confirmedPass"
+              className={styles.registerLabels}
+            >
+              Confirme a senha
+            </FormLabel>
+            <Input
+              isInvalid={errors.confirmedPass ? true : false}
+              {...register("confirmedPass", {
+                ...errorValidation.password,
+                ...errorValidation.filled,
+              })}
+              id="confirmedPass"
+              type="password"
+            />
+            {!errors.password ? (
+              <FormHelperText>
+                A senha deve conter no mínimo 8 caracteres.
+              </FormHelperText>
+            ) : (
+              <FormErrorMessage>{errors.password.message}</FormErrorMessage>
+            )}
+          </FormControl>
+        </Box>
+      </Flex>
 
-        <FormLabel>Possui deficiência? Se sim, qual?</FormLabel>
-        <Input  
-          {...register('haveDisability')}
-          id='haveDisability'
-          type='text'
+      <FormControl>
+        <FormLabel htmlFor="haveDisability" className={styles.registerLabels}>
+          Possui deficiência? Se sim, qual?
+        </FormLabel>
+        <Input
+          {...register("haveDisability")}
+          id="haveDisability"
+          type="text"
         />
+      </FormControl>
 
-        <FormLabel>É especialista em alguma deficiência? Se sim, qual?</FormLabel>
-        <Input  
-          {...register('isSpecialist')}
-          id='isSpecialist'
-          type='text'
-        />
-        
-        <Checkbox id='checkbox'>
+      <FormControl>
+        <FormLabel htmlFor="isSpecialist" className={styles.registerLabels}>
+          É especialista em alguma deficiência? Se sim, qual?
+        </FormLabel>
+        <Input {...register("isSpecialist")} id="isSpecialist" type="text" />
+      </FormControl>
+
+      <FormControl>
+        <Checkbox
+          id="checkbox"
+          className={styles.registerLabels}
+          {...register("checkbox", { ...errorValidation.filled })}
+        >
           Li e aceito os termos e políticas do Portal Acessibilidade Dev
         </Checkbox>
-        
-        <Input type='submit' value='Enviar' color='blue' />
-     
       </FormControl>
+
+      <Input
+        className={styles.registerLabels}
+        type="submit"
+        value="Enviar"
+        color="blue"
+      />
     </form>
-    
-  )
+  );
 }
 
 export default RegisterForm;
