1df428cf1c740c5f1bcf62292ee10f2586d9db87
Ranayna <ranayna-oliveira@outlook.com>
2023-03-12 21:55:43 -0300

    feat(Navbar):cria elemento de barra de busca

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,24 +1,38 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 import MenuSideBar from '../MenuSideBar/'
 
+import { Search } from "react-feather";
+
+
 import { 
   Box,
+  Button,
   Divider,
   Flex,
   Heading,
+  InputGroup,
+  Input,
+  InputLeftAddon,
+  InputRightElement,
+  Select,
   Spacer,
   Tab,
   Tabs,
   TabList,
-  TabPanel,
-  TabPanels
 
 } from '@chakra-ui/react'
 
 const index = () => {
+
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearchTerm = () => {
+    console.log();
+  }
+
   return (  
-    <Box h='115' w='100%' bg='gray.400' >
+    <Box  w='100%' bg='gray.400' >
       <Flex justify="center">
         <Spacer/>
         <Heading as='h4' size='md'>
@@ -28,28 +42,35 @@ const index = () => {
         <MenuSideBar/>
       </Flex>
       <Divider orientation='horizontal'/>
-        <Tabs>
-          <TabList>
-            <Tab color='red' fontWeight='bold'>Artigos</Tab>
-            <Tab color='green' fontWeight='bold'>Ferramentas</Tab>
-            <Tab color='blue' fontWeight='bold'>Fórum</Tab>
-            <Tab color='yellow' fontWeight='bold'>Notícias</Tab>
-          </TabList>
-              <TabPanels>
-                <TabPanel>
-                  <p>Link para pagina artigos</p>
-                </TabPanel>  
-                <TabPanel>
-                  <p>Link para pagina ferramentas</p>
-                </TabPanel>
-                <TabPanel>
-                  <p>Link para pagina fórum</p>
-                </TabPanel>
-                <TabPanel>
-                  <p>Link para pagina notícias</p>
-                </TabPanel>
-              </TabPanels>
-      </Tabs>
+      <Flex pt={3} pr={2}>
+        <Tabs flex={1}>
+            <TabList>
+              <Tab color='red' fontWeight='bold'>Artigos</Tab>
+              <Tab color='green' fontWeight='bold'>Ferramentas</Tab>
+              <Tab color='blue' fontWeight='bold'>Fórum</Tab>
+              <Tab color='yellow' fontWeight='bold'>Notícias</Tab>
+            </TabList>
+        </Tabs>
+        <Spacer />
+        <Box flex={2}>
+          <InputGroup>
+            <InputLeftAddon>
+            <Select  variant='flushed'>
+              <option value='Artigos'>Artigos</option>
+              <option value='Ferramentas'>Ferramentas</option>
+              <option value='Fórum'>Fórum</option>
+              <option value='Notícias'>Notícias</option>
+            </Select>
+          </InputLeftAddon>
+          <Input type='text' name='searchTerm' value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)}/>
+          <InputRightElement>
+            <Button variant='outline' p={2}>
+              <Search color='#fff' size={48}/>
+            </Button>
+          </InputRightElement>
+          </InputGroup>
+        </Box>
+      </Flex>
     </Box>
   )
 }
