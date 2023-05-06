44740420a91b2778a69f6e7270a6b1dce5237675
Luan Teixeira <luanlts@outlook.com>
2023-05-05 21:38:01 -0300

    feat(ListTopics): estrutura dos filtros por categorias

--- src/pages/Forum/ListTopics/index.jsx
+++ src/pages/Forum/ListTopics/index.jsx
@@ -1,4 +1,12 @@
-import { Box, Flex, Heading } from "@chakra-ui/react";
+import {
+  Alert,
+  Box,
+  Flex,
+  Heading,
+  Tag,
+  TagLabel,
+  TagRightIcon,
+} from "@chakra-ui/react";
 import React from "react";
 
 import TextCard from "../../../components/TextCard";
@@ -38,23 +46,53 @@ const ListTopics = () => {
       {isPending ? (
         <SpinnerLoading />
       ) : error ? (
-        JSON.stringify(error)
+        (() => {
+          console.error(error);
+          return (
+            <Alert status="error" mt={10}>
+              Houve um erro ao tentar busca o tópico, por favor, tente
+              novamente!
+            </Alert>
+          );
+        })()
       ) : (
-        <div className="grid gap-2 md:grid-cols-2 sm:grid-cols-1">
-          {data.message.map((t) => (
-            <Box px={10} py={3} key={t.id}>
-              <Link to={`/forum/topico/${t.id}`}>
-                <TextCard
-                  title={t.title}
-                  body={t.description}
-                  date_published={t.date_published}
-                  replies={t.replies}
-                  author={t.author}
-                />
-              </Link>
-            </Box>
-          ))}
-        </div>
+        <>
+          <div>
+            <Tag size="lg" rounded="full" px={5} bg="gray.400" as="button">
+              <TagLabel>Visual</TagLabel>
+              <Tag rounded="full" bg="gray.300" ml={3}>
+                10
+              </Tag>
+            </Tag>
+            <Tag size="lg" rounded="full" px={5} bg="gray.200">
+              <TagLabel>Visual</TagLabel>
+              <Tag rounded="full" bg="gray.300" ml={3}>
+                10
+              </Tag>
+            </Tag>
+            <Tag size="lg" rounded="full" px={5} bg="gray.200">
+              <TagLabel>Visual</TagLabel>
+              <Tag rounded="full" bg="gray.300" ml={3}>
+                10
+              </Tag>
+            </Tag>
+          </div>
+          <div className="grid gap-2 md:grid-cols-2 sm:grid-cols-1">
+            {data.message.map((t) => (
+              <Box px={10} py={3} key={t.id}>
+                <Link to={`/forum/topico/${t.id}`}>
+                  <TextCard
+                    title={t.title}
+                    body={t.description}
+                    date_published={t.date_published}
+                    replies={t.replies}
+                    author={t.author}
+                  />
+                </Link>
+              </Box>
+            ))}
+          </div>
+        </>
       )}
     </div>
   );
