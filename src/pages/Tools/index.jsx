ee94d74de1164df4ef4a888ac0db63dc49e9c3ac
Marcelo Daniliauskas <marcelo.daniliauskas@gmail.com>
2023-05-29 22:10:21 -0300

    feat: ajustando rota de ferramentas e criação inicial da página

---
+++ src/pages/Tools/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Tools = () => {    
+  return (
+    <div>
+      <h1>Ferramentas</h1>
+    </div>
+  );
+}
+
+export default Tools;
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -16,12 +16,13 @@ import NewChat from "../pages/Chat/NewChat/index.jsx";
 import ChatDetails from "../pages/Chat/ChatDetails/index.jsx";
 import ListChats from "../pages/Chat/ListChats/index.jsx";
 import Chat from "../pages/OpenIA/Chat";
+import Tools from "../pages/Tools";
 
 export default createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Navbar />}>
       <Route path="/artigos" element={<p>Artigos</p>} />
-      <Route path="/ferramentas" element={<p>Ferramentas</p>} />
+      <Route path="/ferramentas" element={<Tools />} />
 
       <Route path="/forum" element={<ListTopics />} />
       <Route path="/forum/novo-topico" element={<NewTopic />} />
