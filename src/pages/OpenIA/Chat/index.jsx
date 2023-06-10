fb5e7f5e6861125322e632a29376a471e55ef41d
Arthur Nascimento <arthur.nascimento2020@outlook.com>
2023-06-09 23:47:03 -0300

    feat: add doubts styling, structure and responsive

--- src/pages/OpenIA/Chat/index.jsx
+++ src/pages/OpenIA/Chat/index.jsx
@@ -1,79 +1,133 @@
-import { React, useRef, useState } from 'react'
-import { Box, Button, Heading, Input } from "@chakra-ui/react";
+import { React, useRef, useState } from "react";
+import { Box, Button, Heading, Input, Spinner, Text } from "@chakra-ui/react";
 import { Send } from "react-feather";
-
 
 const Chat = () => {
   const inputRef = useRef();
+
   const [result, setResult] = useState("");
-  const handleSendMessage = async (e) => {
-    e.preventDefault();
-    fetch('https://acessibilidade-dev-back-end.herokuapp.com/openai', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        "text": inputRef.current.value
-      })
-    })
-    .then(response => response.json()) 
-    .then(data => {
+  const [question, setQuestion] = useState("");
+  const [status, setStatus] = useState(0);
+
+  const checkShortcut = (e) => {
+    if (!e.shiftKey && e.keyCode === 13) {
+      e.preventDefault();
+      handleSendMessage();
+    }
+  };
+
+  const handleSendMessage = async () => {
+    setQuestion(inputRef.current.value);
+    setResult("");
+    setStatus(0);
+    try {
+      const response = await fetch(
+        "https://acessibilidade-dev-back-end.herokuapp.com/openai",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            text: inputRef.current.value,
+          }),
+        }
+      );
+      inputRef.current.value = "";
+      const data = await response.json();
+      setStatus(response.status);
+      if (!response.ok && response.status === 400) {
+        throw Error("Está dúvida não é sobre acessibilidade digital!");
+      }
       setResult(data.result);
-    })
-    .catch(error => {
-    console.error('Erro:', error);
-  });
-  }
+    } catch (error) {
+      console.error("Erro:", error.message);
+    }
+  };
   return (
-    <div>
-          <Box className="md:container mx-auto">
-            <Box className="flex my-5" justifyContent="space-between">
-              <Heading as="h1" className="capitalize">
-                Open IA Chat
-              </Heading>
-            </Box>
-            <Box
-              border="1px"
-              borderColor="gray.100"
-              borderRadius="xl"
-              padding="15px"
-              my="5px"
-            >
-          <Box>
-            <Box mx="30px" mt="5px" mb="15px">
-              <Heading as="h2" size="md">
-                Olá, seja bem-vindo! 
-              </Heading>
-              <br/>
-              <Heading as="h3" size="md">
-                Meu nome é <strong>OpenIA</strong> e sou um assistente virtual que pode te ajudar a tirar dúvidas sobre acessibilidade.
-              </Heading>
-              <br/>
-              <Heading as="h4" size="md">
-                O que você gostaria de saber?
-              </Heading>
-              <br/>
-                </Box>
+    <Box className="container py-5">
+      <Heading as="h1" className="capitalize title-color mx-2">
+        IA Generativa
+      </Heading>
+      <Box className="row justify-content-center mx-2">
+        <Box className="row bg-white border rounded p-3 my-3">
+          <Box bg="#CDCBCB" className="border rounded p-3 ">
+            <Heading className="title-color" as="h2" size="md">
+              Olá, seja bem-vindo!
+            </Heading>
+            <br />
+            <Heading className="title-color" as="h3" size="md">
+              Essa seção é dedicada a tirar suas dúvidas sobre acessibilidade
+              digital...
+            </Heading>
+            <br />
+            <Heading className="title-color" as="h4" size="md">
+              O que você gostaria de saber?
+            </Heading>
+            <br />
           </Box>
-            {result}
-          <Box>
-          </Box>
-              <Box p={1}>
-            <Input placeholder='Basic usage' ref={inputRef} />
-                <Button
-                  mt={1}
-                  aria-label="Enviar mensagem na sala <Nome da sala>"
-                  rightIcon={<Send />}
-                  onClick={handleSendMessage}
-                >
-                  Enviar
-                </Button>
+          {question !== "" ? (
+            <Box className="mt-4">
+              <Box className="mb-3">
+                <Heading className="mb-2 title-color" as="h4" size="md">
+                  Pergunta:
+                </Heading>
+                <Text className="title-color">{question}</Text>
+              </Box>
+              <Box>
+                <Heading className="mb-2 title-color" as="h4" size="md">
+                  Resultado:
+                </Heading>
+                {status === 0 ? (
+                  <Box className="d-flex justify-content-center my-3">
+                    <Spinner />
+                  </Box>
+                ) : result !== "" ? (
+                  <Text className="title-color">{result}</Text>
+                ) : status === 400 ? (
+                  <Text className="title-color">
+                    Está dúvida não é sobre acessibilidade digital!
+                  </Text>
+                ) : (
+                  <Text className="title-color">
+                    Ocorreu um erro, tente novamente mais tarde!
+                  </Text>
+                )}
               </Box>
             </Box>
-          </Box>
-    </div>
-  )
-}
+          ) : null}
+        </Box>
+        <Box className="row bg-white border rounded p-0 px-3 py-1 border-light-subtle border-1 bg-white">
+          <textarea
+            id="textmessage"
+            className="m-0 p-0 overflow-y-scroll col"
+            style={{
+              maxHeight: 200,
+              height: 24 * 3,
+              outline: "none",
+              resize: "none",
+            }}
+            tabIndex={0}
+            onKeyDown={checkShortcut}
+            placeholder="Digite sua dúvida aqui..."
+            readOnly={question !== "" && result === "" && status === 0}
+            ref={inputRef}
+          />
+          <Button
+            aria-label="Enviar mensagem de duvida"
+            onClick={handleSendMessage}
+            isDisabled={question !== "" && result === "" && status === 0}
+            bgColor="white"
+            _hover="white"
+            className="col-auto p-0 align-self-center"
+            h="24px"
+          >
+            <Send />
+          </Button>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
 
-export default Chat
+export default Chat;
