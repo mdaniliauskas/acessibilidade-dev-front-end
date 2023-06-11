e759249cd3c463d0bcd1af15a147a2e3dfa96a7c
Arthur Nascimento <arthur.nascimento2020@outlook.com>
2023-06-10 21:53:09 -0300

    feat: add default styling in register form

--- src/pages/Register/RegisterForm.jsx
+++ src/pages/Register/RegisterForm.jsx
@@ -1,5 +1,5 @@
 import { useForm } from "react-hook-form";
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 
 import CustomButton from "../../components/CustomButton";
 
@@ -21,7 +21,6 @@ import {
   Input,
 } from "@chakra-ui/react";
 import { useAuth0 } from "@auth0/auth0-react";
-
 
 function RegisterForm({ handlePorcentage }) {
   const { user } = useAuth0();
@@ -67,25 +66,6 @@ function RegisterForm({ handlePorcentage }) {
       user.completedProfile = true;
       navigate("/", { replace: true });
     }
-
-    // fetch(SIGNUP, {
-    //   mode: "no-cors",
-    //   method: "POST",
-    // headers: {
-    //   "Content-Type": "application/json",
-    //   "Access-Control-Allow-Origin": "*",
-    // },
-    //   body: JSON.stringify(formData),
-    // })
-    //   .then((res) => {
-    //     if (!res.ok) throw Error("Deu ruim");
-    //     return res.json();
-    //   })
-    //   .then((data) => {
-    //     console.log("Cadastrou o usuário");
-    //     console.log("Usuário cadastrado:", data);
-    //   })
-    //   .catch((err) => console.error("Deu erro na requisicao ", err));
   };
 
   useEffect(() => {
@@ -97,89 +77,101 @@ function RegisterForm({ handlePorcentage }) {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <Box className='container py-5'>
-        <FormControl isRequired isInvalid={errors.first_name}>
-          <FormLabel htmlFor="first_name" className={styles.registerLabels}>
-            Nome
-          </FormLabel>
-          <Input
-            id="firstName"
-            placeholder="Digite o seu nome"
-            isInvalid={errors.first_name ? true : false}
-            {...register("first_name", {
-              ...errorValidation.names,
-              ...errorValidation.filled,
-            })}
-          />
-          {!errors.first_name ? (
-            <FormHelperText>
-              O campo deve possui no mínimo 2 de caracteres.
-            </FormHelperText>
-          ) : (
-            <FormErrorMessage>{errors.first_name.message}</FormErrorMessage>
-          )}
-        </FormControl>
+      <FormControl
+        className="bg-white border rounded p-3 my-3"
+        isRequired
+        isInvalid={errors.first_name}
+      >
+        <FormLabel className="title-color" htmlFor="first_name">
+          Nome
+        </FormLabel>
+        <Input
+          id="firstName"
+          placeholder="Digite o seu nome"
+          isInvalid={errors.first_name ? true : false}
+          {...register("first_name", {
+            ...errorValidation.names,
+            ...errorValidation.filled,
+          })}
+        />
+        {!errors.first_name ? (
+          <FormHelperText>
+            O campo deve possui no mínimo 2 de caracteres.
+          </FormHelperText>
+        ) : (
+          <FormErrorMessage>{errors.first_name.message}</FormErrorMessage>
+        )}
+      </FormControl>
 
-        <FormControl isRequired isInvalid={errors.last_name}>
-          <FormLabel htmlFor="lastName" className={styles.registerLabels}>
-            Sobrenome
-          </FormLabel>
-          <Input
-            id="lastName"
-            placeholder="Digite o seu sobrenome"
-            isInvalid={errors.last_name ? true : false}
-            {...register("last_name", {
-              ...errorValidation.names,
-              ...errorValidation.filled,
-            })}
-          />
-          {!errors.last_name ? (
-            <FormHelperText>
-              O campo deve possui no mínimo 2 de caracteres.
-            </FormHelperText>
-          ) : (
-            <FormErrorMessage>{errors.last_name.message}</FormErrorMessage>
-          )}
-        </FormControl>
+      <FormControl
+        className="bg-white border rounded p-3 mb-3"
+        isRequired
+        isInvalid={errors.last_name}
+      >
+        <FormLabel className="title-color" htmlFor="lastName">
+          Sobrenome
+        </FormLabel>
+        <Input
+          id="lastName"
+          placeholder="Digite o seu sobrenome"
+          isInvalid={errors.last_name ? true : false}
+          {...register("last_name", {
+            ...errorValidation.names,
+            ...errorValidation.filled,
+          })}
+        />
+        {!errors.last_name ? (
+          <FormHelperText>
+            O campo deve possui no mínimo 2 de caracteres.
+          </FormHelperText>
+        ) : (
+          <FormErrorMessage>{errors.last_name.message}</FormErrorMessage>
+        )}
+      </FormControl>
 
-        <FormControl isRequired isInvalid={errors.birth_date}>
-          <FormLabel htmlFor="birth_date">Data de Nascimento</FormLabel>
-          <Input
-            id="birth_date"
-            type="date"
-            isInvalid={errors.birth_date ? true : false}
-            {...register("birth_date", {
-              ...errorValidation.filled,
-            })}
-          />
-        </FormControl>
+      <FormControl
+        className="bg-white border rounded p-3 mb-3"
+        isRequired
+        isInvalid={errors.birth_date}
+      >
+        <FormLabel className="title-color" htmlFor="birth_date">
+          Data de Nascimento
+        </FormLabel>
+        <Input
+          id="birth_date"
+          type="date"
+          isInvalid={errors.birth_date ? true : false}
+          {...register("birth_date", {
+            ...errorValidation.filled,
+          })}
+        />
+      </FormControl>
 
-        <FormControl>
-          <FormLabel htmlFor="disability" className={styles.registerLabels}>
-            Possui deficiência? Se sim, qual?
-          </FormLabel>
-          <Input {...register("disability")} id="disability" type="text" />
-        </FormControl>
+      <FormControl className="bg-white border rounded p-3 mb-3">
+        <FormLabel className="title-color" htmlFor="disability">
+          Possui deficiência? Se sim, qual?
+        </FormLabel>
+        <Input {...register("disability")} id="disability" type="text" />
+      </FormControl>
 
-        <FormControl>
-          <FormLabel htmlFor="specialist_area" className={styles.registerLabels}>
-            É especialista em alguma deficiência? Se sim, qual?
-          </FormLabel>
-          <Input {...register("specialist_area")} id="isSpecialist" type="text" />
-        </FormControl>
+      <FormControl className="bg-white border rounded p-3 mb-3">
+        <FormLabel className="title-color" htmlFor="specialist_area">
+          É especialista em alguma deficiência? Se sim, qual?
+        </FormLabel>
+        <Input {...register("specialist_area")} id="isSpecialist" type="text" />
+      </FormControl>
 
-        <FormControl>
-          <Checkbox
-            id="checkbox"
-            className={styles.registerLabels}
-            {...register("checkbox", { ...errorValidation.filled })}
-          >
-            Li e aceito os termos e políticas do Portal Acessibilidade Dev
-          </Checkbox>
-        </FormControl>
+      <FormControl className="bg-white border rounded p-3 mb-3">
+        <Checkbox
+          id="checkbox"
+          className="title-color"
+          {...register("checkbox", { ...errorValidation.filled })}
+        >
+          Li e aceito os termos e políticas do Portal Acessibilidade Dev
+        </Checkbox>
+      </FormControl>
 
-        <CustomButton className={styles.registerLabels}>Cadastrar</CustomButton>
-      </Box>
+      <CustomButton>Cadastrar</CustomButton>
     </form>
   );
 }
