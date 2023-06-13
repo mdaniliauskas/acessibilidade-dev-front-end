b510183ba8207c4bf197c1d73d6af367882d7c2c
Arthur Nascimento <arthur.nascimento2020@outlook.com>
2023-06-12 21:53:10 -0300

    refactor: ajuste na rota raiz
    agora a rota de raiz é o forum

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,7 +1,10 @@
 import {
   createBrowserRouter,
   createRoutesFromElements,
+  Outlet,
+  BrowserRouter,
   Route,
+  Routes,
 } from "react-router-dom";
 
 // navbar fixo para todas as paginas
@@ -22,28 +25,22 @@ import LoginRedirect from "../pages/LoginRedirect";
 export default createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Navbar />}>
+      <Route index path="/" element={<ListTopics />} />
       <Route path="/artigos" element={<p>Artigos</p>} />
       <Route path="/ferramentas" element={<p>Ferramentas</p>} />
-      <Route path="/noticias" element={<p>Noticias</p>} />
-
       <Route path="/forum" element={<ListTopics />} />
+      <Route path="/noticias" element={<p>Noticias</p>} />
 
       {/* Rota privada */}
       <Route path="/forum/novo-topico" element={<NewTopic />} />
-
       <Route path="/forum/topico/:topicId" element={<TopicDetails />} />
       <Route path="/forum/editar-topico/:topicId" element={<UpdateTopic />} />
-
       <Route path="/completarCadastro" element={<Register />} />
-
       <Route path="/chat" element={<ListChats />} />
-
       {/* Rota privada */}
       <Route path="/chat/novo-chat" element={<NewChat />} />
-
       {/* Rota privada */}
       <Route path="/chat/:chatId" element={<ChatDetails />} />
-
       <Route path="/openIA" element={<Chat />} />
     </Route>
   )
